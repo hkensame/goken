071fc8e5ef078fc268d0d836a38b47cb697a9902
ken <3088961181@qq.com>
2025-03-24 10:41:00 +0800

    update

--- pkg/time/timer.go
+++ pkg/time/timer.go
@@ -10,27 +10,46 @@ type ResettableTimer struct {
 	mu      sync.Mutex
 	timer   *time.Timer
 	period  time.Duration
+	resetCh chan struct{}
+	//是否被关闭
 	closed  bool
 	fn      func() bool
 	doFirst bool
+	tfn     func() time.Duration
 }
 
 type OptionFunc func(*ResettableTimer)
 
-// 创建新的可重置定时器,该定时器在不调用reset的情况下每隔一次指定period时间段都将运行一次fn,不会停止
+// 创建新的可重置定时器,该定时器在不调用reset的情况下每隔一次指定period时间段都将运行一次fn,
+// fn的类型是func()bool,若fn返回false则停止定时
 // 如果调用了reset则重新进入等待而不是类似官方timer那样触发fn
 func MustNewResettableTimer(period time.Duration, fn func() bool, opts ...OptionFunc) *ResettableTimer {
 	rt := &ResettableTimer{
-		period: period,
-		fn:     fn,
-		closed: false,
+		period:  period,
+		fn:      fn,
+		resetCh: make(chan struct{}),
 	}
+
+	for _, opt := range opts {
+		opt(rt)
+	}
+
 	rt.timer = time.NewTimer(period)
+	rt.tfn = func() time.Duration {
+		return period
+	}
 	return rt
 }
 
 func (rf *ResettableTimer) Run() {
-	go rf.runLoop()
+	rf.mu.Lock()
+	defer rf.mu.Unlock()
+	//允许stop后重启,如果发现timer是关闭的则重新启动
+	if rf.closed {
+		rf.resetCh = make(chan struct{})
+		rf.closed = false
+		go rf.runLoop()
+	}
 }
 
 // 运行定时器的主循环
@@ -43,22 +62,42 @@ func (rt *ResettableTimer) runLoop() {
 	}
 	rt.mu.Unlock()
 
-	for rt.closed {
-		<-rt.timer.C
-		rt.mu.Lock()
-		if !rt.closed {
+	for !rt.closed {
+		select {
+		case <-rt.timer.C:
+			rt.mu.Lock()
+			if rt.closed {
+				rt.mu.Unlock()
+				return
+			}
+
+			//执行一次定时任务
+			if !rt.fn() {
+				rt.closed = true
+			} else {
+				//如果运行fn成功则重新开始定时运行任务
+				rt.timer.Stop()
+				rt.timer = time.NewTimer(rt.tfn())
+			}
 			rt.mu.Unlock()
-			return
+		case <-rt.resetCh:
+			//如果reset函数触发则重置timer,老的timer直接忽视
+			rt.mu.Lock()
+			if rt.closed {
+				rt.mu.Unlock()
+				return
+			}
+
+			rt.timer.Stop()
+			rt.timer = time.NewTimer(rt.tfn())
+			rt.mu.Unlock()
 		}
-		rt.fn()
-		rt.timer.Reset(rt.period)
-		rt.mu.Unlock()
+
 	}
 }
 
 // 重新开始定时器
 func (rt *ResettableTimer) Reset() {
-	rt.mu.Lock()
 	rt.ResetWithTTK(rt.period)
 }
 
@@ -66,17 +105,14 @@ func (rt *ResettableTimer) ResetWithTTK(t time.Duration) {
 	rt.mu.Lock()
 	defer rt.mu.Unlock()
 
-	if !rt.closed {
+	if rt.closed {
 		return
 	}
 
-	if !rt.timer.Stop() {
-		select {
-		case <-rt.timer.C: // 清空过期的定时器信号
-		default:
-		}
+	select {
+	case rt.resetCh <- struct{}{}:
+	default:
 	}
-	rt.timer.Reset(t)
 }
 
 // 停止定时器
@@ -84,12 +120,21 @@ func (rt *ResettableTimer) Stop() {
 	rt.mu.Lock()
 	defer rt.mu.Unlock()
 	rt.closed = true
+	//关闭channel间接使已失效的runLoop关闭
+	close(rt.resetCh)
 	rt.timer.Stop()
 }
 
-// 让整个定时器先执行一次传入的func再进行定时
+// 让整个定时器先执行一次传入的func再进行定时,注意如果关闭了定时器再开启会再次判断一次doFirst
 func (rt *ResettableTimer) WithDoFirst(do bool) OptionFunc {
 	return func(rt *ResettableTimer) {
 		rt.doFirst = do
+	}
+}
+
+// 允许让fn生成在时间区间内随机的等待时间
+func (rt *ResettableTimer) WithRandomTimer(fn func() time.Duration) OptionFunc {
+	return func(rt *ResettableTimer) {
+		rt.tfn = fn
 	}
 }
